e2f06aedc34f1de654fb115ac41e638a41a85a26
Warren Rawlings <rawlings.warren@gmail.com>
2019-10-19 18:13:50 -0400

    got help from Angel and did Numbers

--- src/components/ButtonComponents/NumberButtons/Numbers.js
+++ src/components/ButtonComponents/NumberButtons/Numbers.js
@@ -1,5 +1,5 @@
-import React from "react";
-
+import React, { useState } from "react";
+import NumberButton from "./NumberButton";
 //import any components needed
 
 //Import your array data to from the provided data file
@@ -8,15 +8,16 @@ import { numbers } from "../../../data";
 const Numbers = () => {
   // STEP 2 - add the imported data to state
   ////Warren: setState is tracking the activity inside numberState and setNumberState for changes
-  ////Warren: So what is numbers inside the argument at setState?
-  const [numberState, setNumberState] = setState(numbers);
+  ////Warren: So what is numbers inside the argument at setState? The original array?
+  const [data, setData] = useState(numbers);
 
   return (
     <div>
       {/* STEP 3 - Use .map() to iterate over your array data and return a button
        component matching the name on the provided file. Pass
        it any props needed by the child component*/}
-       {let numberState = numbers.map();}
+       {data.map((number) => <NumberButton number={number}/>)}
     </div>
   );
 };
+export default Numbers;

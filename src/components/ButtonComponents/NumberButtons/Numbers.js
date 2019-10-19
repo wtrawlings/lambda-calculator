393569fd064d81f9322d6fcedfe224d1ca5a3e82
Warren Rawlings <rawlings.warren@gmail.com>
2019-10-19 08:14:53 -0400

    still working on step 3 and a little confused

--- src/components/ButtonComponents/NumberButtons/Numbers.js
+++ src/components/ButtonComponents/NumberButtons/Numbers.js
@@ -16,6 +16,7 @@ const Numbers = () => {
       {/* STEP 3 - Use .map() to iterate over your array data and return a button
        component matching the name on the provided file. Pass
        it any props needed by the child component*/}
+       {let numberState = numbers.map();}
     </div>
   );
 };
